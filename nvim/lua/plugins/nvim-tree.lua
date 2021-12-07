5c746c86941e33e90ea089f60acbcc120e9e58c3
amei <cms1984@msn.com>
2021-12-07 16:58:07 +0800

    nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,9 @@ vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeFindFile<CR>', {
 	noremap = true,
 	silent = true
 })
+vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_hide_dotfiles = 1
+vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_side = 'left'
 vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_gitignore = 1
