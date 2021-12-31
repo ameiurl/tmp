87729e3319dcb7e2b424809c75527ea764fc7f27
amei <cms1984@msn.com>
2021-12-31 14:14:49 +0800

    nvim tree width

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -10,7 +10,7 @@ vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeFindFile<CR>', {
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_side = 'left'
-vim.g.nvim_tree_width = 40
+vim.g.nvim_tree_width = 45
 vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_quit_on_open = 0
 -- vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
