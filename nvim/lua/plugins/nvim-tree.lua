663c2e32d76c2230e3ee2fc7d1acffa44fd836df
amei <cms1984@msn.com>
2022-03-06 08:31:47 +0800

    nvim_tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -7,11 +7,11 @@ vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeFindFile<CR>', {
 	silent = true
 })
 -- vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_hide_dotfiles = 1
+-- vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_side = 'left'
 vim.g.nvim_tree_width = 45
-vim.g.nvim_tree_gitignore = 1
+-- vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_quit_on_open = 0
 -- vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 vim.g.nvim_tree_show_icons = {
