3e233865ea875e7428de5662e04daf8999ba4639
amei <cms1984@msn.com>
2022-05-30 09:42:51 +0800

    keymap

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -30,7 +30,7 @@ require "nvim-treesitter.configs".setup {
 	    enable = true 
 	},
 	incremental_selection = {
-		enable = true,
+		enable = false,
 		keymaps = {
 			init_selection = "<CR>", -- maps in normal mode to init the node/scope selection
 			node_incremental = "<CR>", -- increment to the upper named parent
