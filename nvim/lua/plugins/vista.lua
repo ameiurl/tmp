83d7276c164b4ccd926c402c4a060acf8cf0e1ca
amei <cms1984@msn.com>
2022-11-23 14:32:00 +0800

    fix

--- nvim/lua/plugins/vista.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd [[
-  nnoremap <silent> <Leader>t :Vista!!<CR>
-  let g:vista_ignore_kinds = ['Variable']
-]]
-
-vim.g.vista_default_executive = 'nvim_lsp'
--- zsh/plugins.zsh
+++
@@ -1,9 +0,0 @@
-export PLUG_DIR=$HOME/.zim
-if [[ ! -d $PLUG_DIR ]]; then
-	curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-	# curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/zimfw/install/master/install.zsh
-	rm ~/.zimrc
-	ln -s ~/.config/zsh/zimrc ~/.zimrc
-	source ~/.zim/zimfw.zsh
-	zimfw install
-fi
